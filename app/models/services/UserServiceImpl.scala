2b36a454e41ebeff3d29f51943bef37d10d9733a
tomoyaueno29 <u.t012807@gmail.com>
2020-08-23 09:56:20 +0900

    Create UserServiceImpl.scala

---
+++ app/models/services/UserServiceImpl.scala
@@ -0,0 +1,88 @@
+package models.services
+
+import java.util.UUID
+import javax.inject.Inject
+
+import com.mohiva.play.silhouette.api.LoginInfo
+import com.mohiva.play.silhouette.impl.providers.CommonSocialProfile
+import models.User
+import models.daos.UserDAO
+import play.api.libs.concurrent.Execution.Implicits._
+
+import scala.concurrent.Future
+
+/**
+ * Handles actions to users.
+ *
+ * @param userDAO The user DAO implementation.
+ */
+class UserServiceImpl @Inject() (userDAO: UserDAO) extends UserService {
+
+  /**
+   * Retrieves a user that matches the specified login info.
+   *
+   * @param loginInfo The login info to retrieve a user.
+   * @return The retrieved user or None if no user could be retrieved for the given login info.
+   */
+  def retrieve(loginInfo: LoginInfo): Future[Option[User]] = userDAO.find(loginInfo)
+
+  /**
+   * Retrives a user that matches the specified userId.
+   * @param userId The user id to retriece a user.
+   * @return The retrieved user or None if no user could be retrieved for the given user id.
+   */
+  def retrieve(userId: UUID): Future[Option[User]] = userDAO.find(userId)
+
+  /**
+   * Saves a user.
+   *
+   * @param user The user to save.
+   * @return The saved user.
+   */
+  def save(user: User) = {
+    userDAO.find(user.userID).flatMap{
+      case Some(u) => // Update except User.userID
+        userDAO.update(u.copy(
+          loginInfo = user.loginInfo,
+          firstName = user.firstName,
+          lastName = user.lastName,
+          fullName = user.fullName,
+          email = user.email,
+          avatarURL = user.avatarURL,
+          mailConfirmed = user.mailConfirmed))
+      case None => userDAO.save(user)
+    }
+  }
+
+  /**
+   * Saves the social profile for a user.
+   *
+   * If a user exists for this profile then update the user, otherwise create a new user with the given profile.
+   *
+   * @param profile The social profile to save.
+   * @return The user for whom the profile was saved.
+   */
+  def save(profile: CommonSocialProfile) = {
+    userDAO.find(profile.loginInfo).flatMap {
+      case Some(user) => // Update user with profile
+        userDAO.save(user.copy(
+          firstName = profile.firstName,
+          lastName = profile.lastName,
+          fullName = profile.fullName,
+          email = profile.email,
+          avatarURL = profile.avatarURL
+        ))
+      case None => // Insert a new user
+        userDAO.save(User(
+          userID = UUID.randomUUID(),
+          loginInfo = profile.loginInfo,
+          firstName = profile.firstName,
+          lastName = profile.lastName,
+          fullName = profile.fullName,
+          email = profile.email,
+          avatarURL = profile.avatarURL,
+          mailConfirmed = None
+        ))
+    }
+  }
+}
