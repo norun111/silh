b47f49ae170f9240d0f909dc0aacc38b9161ce99
tomoyaueno29 <u.t012807@gmail.com>
2020-09-03 00:36:47 +0900

    Update Goal.scala

--- app/models/Goal.scala
+++ app/models/Goal.scala
@@ -1,6 +1,14 @@
 package models
 
+import play.api.libs.json._
+
 case class Goal(
-               name: String,
-               learning_time: Int
-               )
+  id: Int,
+  name: String,
+  learning_time: Int
+)
+
+object Goal {
+  implicit val jsonFormat = Json.format[Goal]
+}
+
