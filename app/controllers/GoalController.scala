4c9954b4d9fcb6f9a4c1cf35bf30201102a1c007
tomoyaueno29 <u.t012807@gmail.com>
2020-09-03 19:19:02 +0900

    Update GoalController.scala

--- app/controllers/GoalController.scala
+++ app/controllers/GoalController.scala
@@ -1,10 +1,18 @@
 package controllers
 
-import models.User
+import models._
 import play.api.mvc._
+import play.api._
+import play.api.mvc._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json._
+import models._
+import reactivemongo.api._
+import play.modules.reactivemongo.MongoController
+import play.modules.reactivemongo.json.collection.JSONCollection
 
-object GoalController extends Controller {
+class GoalController extends Controller {
 
-  def index(user: User) = TODO
-  
+  def index = TODO
+
 }
