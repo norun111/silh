fed5cd1a2d161dccd1a9b735be1c4fb9768c7ee2
tomoyaueno29 <u.t012807@gmail.com>
2020-09-04 19:24:42 +0900

    Create Helpers.scala

---
+++ app/models/daos/Helpers.scala
@@ -0,0 +1,21 @@
+package models.daos
+
+import java.util.concurrent.TimeUnit
+
+import org.mongodb.scala.{ Completed, Document, Observable }
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+object Helpers {
+
+  implicit class DocumentObservable[C](val observable: Observable[Document]) extends ImplicitObservable[Document]
+
+  implicit class CompletedObservable[C](val observable: Observable[Completed]) extends ImplicitObservable[Completed]
+
+  trait ImplicitObservable[C] {
+    val observable: Observable[C]
+
+    def results: Seq[C] = Await.result(observable.toFuture(), Duration(10, TimeUnit.SECONDS))
+  }
+}
