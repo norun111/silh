b839b34d14402d59c3e2c5ad854b75c28bd96d8d
tomoyaueno29 <u.t012807@gmail.com>
2020-09-05 01:22:33 +0900

    Update GoalController.scala

--- app/controllers/GoalController.scala
+++ app/controllers/GoalController.scala
@@ -6,12 +6,12 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json._
-import javax.inject.{Inject, Singleton}
+import javax.inject.{ Inject, Singleton }
 import models.daos.GoalDAO._
 import reactivemongo.api._
-import scala.concurrent.Future
+
+import scala.concurrent.{ ExecutionContext, Future }
 import play.modules.reactivemongo._
-import play.modules.reactivemongo.json.collection.JSONCollection
 import reactivemongo.api.commands.bson.BSONCountCommandImplicits._
 import org.mongodb.scala._
 import org.mongodb.scala.model.Updates._
@@ -21,14 +21,16 @@ import models.daos.Helpers._
 import org.mongodb.scala.model.Projections
 import repositories.GoalRepository
 
-@Singleton
-class GoalController @Inject()(goalRepo: GoalRepository) extends Controller {
+class GoalController @Inject() (
+    ec: ExecutionContext,
+    goalRepo: GoalRepository
+) extends Controller {
 
-  def collection: reactivemongo.play.json.collection.JSONCollection = db.collection[reactivemongo.play.json.collection.JSONCollection]("goal")
-
-  def index = Action {
-    Ok(views.html.goals.index(goals.find.results))
-  }
+  //  def collection: reactivemongo.play.json.collection.JSONCollection = db.collection[reactivemongo.play.json.collection.JSONCollection]("goal")
+  //
+  //  def index = Action {
+  //    Ok(views.html.goals.index(goals.find.results))
+  //  }
 
   def listGoals = Action.async {
     goalRepo.list().map {
