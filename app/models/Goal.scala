8056444915ca25a4dfee330918373dae1ba5b8d0
tomoyaueno29 <u.t012807@gmail.com>
2020-09-05 00:14:26 +0900

    Update Goal.scala

--- app/models/Goal.scala
+++ app/models/Goal.scala
@@ -1,8 +1,11 @@
 package models
 
 import play.api.libs.json._
+import reactivemongo.play.json._
+import reactivemongo.bson.BSONObjectID
 
 case class Goal(
+  _id: Option[BSONObjectID],
   name: String,
   learning_time: Int,
   challengers_num: Int
