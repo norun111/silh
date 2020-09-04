b48d5800d0770672edf67aa510a81e593a712a23
tomoyaueno29 <u.t012807@gmail.com>
2020-09-04 19:24:47 +0900

    Create Helpers.scala

---
+++ test/Helpers.scala
@@ -0,0 +1,21 @@
+package test
+
+import java.util.concurrent.TimeUnit
+
+import org.mongodb.scala.{Completed, Document, Observable}
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+object Helpers {
+
+  implicit class DocumentObservable[C](val observable: Observable[Document]) extends ImplicitObservable[Document]
+
+  implicit class CompletedObservable[C](val observable: Observable[Completed]) extends ImplicitObservable[Completed]
+
+  trait ImplicitObservable[C] {
+    val observable: Observable[C]
+
+    def results: Seq[C] = Await.result(observable.toFuture(), Duration(10, TimeUnit.SECONDS))
+  }
+}
