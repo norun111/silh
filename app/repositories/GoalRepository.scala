c1865af7ea7bf9e45721a985e95c1b9226d4be74
tomoyaueno29 <u.t012807@gmail.com>
2020-09-05 00:43:59 +0900

    Update GoalRepository.scala

--- app/repositories/GoalRepository.scala
+++ app/repositories/GoalRepository.scala
@@ -1,14 +1,19 @@
 package repositories
 
+import java.util.UUID
+
 import javax.inject.Inject
 import play.modules.reactivemongo.ReactiveMongoApi
 import reactivemongo.play.json.collection.JSONCollection
 import reactivemongo.play.json._
-import scala.concurrent.{Future, ExecutionContext}
+
+import scala.concurrent.{ExecutionContext, Future}
 import models.Goal
+import reactivemongo.bson.BSONObjectID
 import org.mongodb.scala.ReadPreference
 import reactivemongo.bson.BSONDocument
 import reactivemongo.api._
+import reactivemongo.api.commands.WriteResult
 
 class GoalRepository @Inject()(
                               implicit ec: ExecutionContext,
@@ -22,4 +27,25 @@ class GoalRepository @Inject()(
         .cursor[Goal]().collect[Seq](limit, Cursor.FailOnError[Seq[Goal]]())
     )
 
+  def create(goal: Goal): Future[WriteResult] =
+    collection.flatMap((_.insert(goal)))
+
+  def read(id: BSONObjectID): Future[Option[Goal]] =
+    collection.flatMap(_.find(BSONDocument("_id" -> id)).one[Goal])
+
+  def update(id: BSONObjectID, goal: Goal): Future[Option[Goal]] =
+    collection.flatMap(_.findAndUpdate(BSONDocument("_id" -> id),
+      BSONDocument(
+        f"$$set" -> BSONDocument(
+          "name" -> goal.name,
+          "learning_time" -> goal.learning_time,
+          "challengers_num" -> goal.challengers_num
+        )
+      ),
+      true
+    ).map(_.result[Goal]))
+
+  def destroy(id: BSONObjectID): Future[Option[Goal]] =
+    collection.flatMap(_.findAndRemove(BSONDocument("_id" ->
+    id)).map(_.result[Goal]))
 }
