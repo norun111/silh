d905f1803c5ee0b9071bd24540d337d916e44b5f
tomoyaueno29 <u.t012807@gmail.com>
2020-09-05 20:40:29 +0900

    Update GoalController.scala

--- app/controllers/GoalController.scala
+++ app/controllers/GoalController.scala
@@ -1,22 +1,11 @@
 package controllers
 
 import models._
-import play.api._
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json._
 import javax.inject._
-import reactivemongo.api._
-
 import scala.concurrent.{ ExecutionContext, Future }
-import play.modules.reactivemongo._
-//import reactivemongo.api.commands.bson.BSONCountCommandImplicits._
-import org.mongodb.scala._
-import org.mongodb.scala.model.Updates._
-import org.mongodb.scala.model.Filters._
-//import reactivemongo.bson._
-import models.daos.Helpers._
-import org.mongodb.scala.model.Projections
 import reactivemongo.bson.BSONObjectID
 import repositories.GoalRepository
 
@@ -24,12 +13,6 @@ class GoalController @Inject() (
     ec: ExecutionContext,
     goalRepo: GoalRepository
 ) extends Controller {
-
-  //  def collection: reactivemongo.play.json.collection.JSONCollection = db.collection[reactivemongo.play.json.collection.JSONCollection]("goal")
-  //
-  //  def index = Action {
-  //    Ok(views.html.goals.index(goals.find.results))
-  //  }
 
   def listGoals = Action.async {
     goalRepo.list().map {
