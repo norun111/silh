c335a96359fe01ca61a7586a181b536180fc4b1b
tomoyaueno29 <u.t012807@gmail.com>
2020-09-05 20:40:44 +0900

    Create Users_goalRepository.scala

---
+++ app/repositories/Users_goalRepository.scala
@@ -0,0 +1,14 @@
+package repositories
+
+import javax.inject.Inject
+import play.modules.reactivemongo.ReactiveMongoApi
+
+import scala.concurrent.ExecutionContext
+
+class Users_goalRepository  @Inject() (
+                                 implicit
+                                 ec: ExecutionContext,
+                                 reactiveMongoApi: ReactiveMongoApi
+                               ) {
+
+}
