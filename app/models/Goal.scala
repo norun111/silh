aab7bc82951fcdef6d47af29f62bafe4a80d93fc
tomoyaueno29 <u.t012807@gmail.com>
2020-09-05 19:24:09 +0900

    Update Goal.scala

--- app/models/Goal.scala
+++ app/models/Goal.scala
@@ -1,17 +1,38 @@
 package models
 
+import play.api.libs.functional.syntax.unlift
 import play.api.libs.json._
-import reactivemongo.play.json._
 import reactivemongo.bson.BSONObjectID
+//import reactivemongo.play.json.compat._
+import reactivemongo.play.json.BSONFormats
+//import reactivemongo.bson.BSONObjectID
+//import reactivemongo.bson._
+import play.modules.reactivemongo.json._, ImplicitBSONHandlers._
+import scala.util.Try
 
 case class Goal(
-  _id: Option[BSONObjectID],
+  id: Option[BSONObjectID],
   name: String,
   learning_time: Int,
   challengers_num: Int
 )
 
 object Goal {
-  implicit val jsonFormat: OFormat[Goal] = Json.format[Goal]
+  import play.api.libs.json._
+
+  implicit object BSONObjectIDFormat extends Format[BSONObjectID] {
+    def writes(objectId: BSONObjectID): JsValue = JsString(objectId.toString())
+    def reads(json: JsValue): JsResult[BSONObjectID] = json match {
+      case JsString(x) => {
+        val maybeOID: Try[BSONObjectID] = BSONObjectID.parse(x)
+        if (maybeOID.isSuccess) JsSuccess(maybeOID.get) else {
+          JsError("Expected BSONObjectID as JsString")
+        }
+      }
+      case _ => JsError("Expected BSONObjectID as JsString")
+    }
+  }
+
+  implicit val goalFormat: OFormat[Goal] = Json.format[Goal]
 }
 
