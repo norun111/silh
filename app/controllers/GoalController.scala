1c97c5c55fb9cc44ccd5d9ec285f9e00bf063305
tomoyaueno29 <u.t012807@gmail.com>
2020-09-08 17:11:39 +0900

    Update GoalController.scala

--- app/controllers/GoalController.scala
+++ app/controllers/GoalController.scala
@@ -1,27 +1,69 @@
 package controllers
 
+import java.util.UUID
+
 import com.mohiva.play.silhouette.api.Silhouette
 import models._
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json._
 import javax.inject._
+import jdk.nashorn.internal.runtime.options.LoggingOption.LoggerInfo
+import play.api.data.Form
+import play.api.data.Forms._
+
+import scala.concurrent._
+import play.api.i18n.{ I18nSupport, MessagesApi }
+import com.mohiva.play.silhouette.api.{ Identity, LoginInfo }
+
 import scala.concurrent.{ ExecutionContext, Future }
 import reactivemongo.bson.BSONObjectID
 import repositories.GoalRepository
 import utils.auth.DefaultEnv
+import play.modules.reactivemongo._
+import reactivemongo.play.json.collection.JSONCollection
 
 class GoalController @Inject() (
+    val reactiveMongoApi: ReactiveMongoApi,
+    val messagesApi: MessagesApi,
     ec: ExecutionContext,
     silhouette: Silhouette[DefaultEnv],
-    goalRepo: GoalRepository
-) extends Controller {
+    goalRepo: GoalRepository,
+    implicit val webJarAssets: WebJarAssets
+) extends Controller with I18nSupport with MongoController with ReactiveMongoComponents {
+
+  val user_goalForm = Form(
+    mapping(
+      "user_id" -> text,
+      "goal_id" -> ignored(BSONObjectID.generate: BSONObjectID),
+      "learning_time" -> number
+    )(User_goals.apply)(User_goals.unapply)
+  )
+
+//    val userForm = Form(
+//      mapping(
+//        "userID" -> text,
+//        "loginInfo" -> ignored(LoginInfo),
+//        "firstName" -> optional(text),
+//        "lastName" -> optional(text),
+//        "fullName" -> optional(text),
+//        "email" -> optional(text),
+//        "avatarURL" -> optional(text),
+//        "activated" -> boolean,
+//        "goal" -> list(mapping(
+//          "_id" -> ignored(BSONObjectID.generate: BSONObjectID),
+//          "name" -> text,
+//          "learning_time" -> number,
+//          "challengers_num" -> number
+//        )(Goal.apply)(Goal.unapply))
+//      )(User.apply)(User.unapply)
+//    )
 
   def listGoals = silhouette.SecuredAction.async { implicit request =>
     // sort by descending "challengers_num"
     goalRepo.list().map {
       goals =>
-        Ok(views.html.goals.index(goals, request.identity))
+        Ok(views.html.goals.index())
     }
   }
 
@@ -49,6 +91,16 @@ class GoalController @Inject() (
       }
     }.getOrElse(Future.successful(BadRequest("Invalid Json")))
   }
+
+  //  def userUpdate(id: UUID) = Action.async {
+  //    implicit request =>
+  //      employeeForm.bindFromRequest.fold(
+  //        formWithErrors => Future.successful(BadRequest(views.html.edit(id, formWithErrors))),
+  //        employee => {
+  //          val futureUpdateEmployee = collection.update(Json.obj("_id" -> Json.obj("$oid" -> id)), employee.copy(_id = BSONObjectID(id)))
+  //          futureUpdateEmployee.map { result => home }
+  //        })
+  //  }
 
   def deleteGoal(id: BSONObjectID) = Action.async {
     goalRepo.destroy(id).map {
