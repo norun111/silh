110e895b8d0eba50d94f21c1e522975bd6c95018
tomoyaueno29 <u.t012807@gmail.com>
2020-09-20 15:36:13 +0900

    Update UserRepository.scala

--- app/repositories/UserRepository.scala
+++ app/repositories/UserRepository.scala
@@ -1,36 +1,32 @@
 package repositories
 
-import java.util.UUID
-
-import com.mongodb.casbah.Imports.{ MongoConnection, MongoDBObject }
+import com.mongodb.casbah.Imports.{ MongoConnection, MongoDBObject, _ }
+import com.novus.salat._
 import javax.inject.Inject
+import models.{ Goal, User }
 import play.modules.reactivemongo.ReactiveMongoApi
+import reactivemongo.bson.{ BSONDocument, _ }
+import reactivemongo.play.json._
 import reactivemongo.play.json.collection.JSONCollection
 
-import scala.concurrent.{ ExecutionContext, Future }
-import models.{ Goal, User }
-import reactivemongo.bson.BSONObjectID
-import reactivemongo.bson._
-//import com.mongodb.casbah.Imports._
-import play.api.mvc.Action
-import models.UsersGoal
-import reactivemongo.api._
-import reactivemongo.api.commands.WriteResult
-import reactivemongo.play.json._
-import play.api.libs.json._
 import scala.concurrent.ExecutionContext.Implicits.global
-import reactivemongo.bson.BSONDocument
-import reactivemongo.api.collections.bson.BSONCollection
+import scala.concurrent.{ ExecutionContext, Future }
 
 class UserRepository @Inject() (
     implicit
     ec: ExecutionContext,
     reactiveMongoApi: ReactiveMongoApi
 ) {
+
+  implicit val ctx = new Context {
+    val name = "Custom_Classloader"
+    override val typeHintStrategy = StringTypeHintStrategy(when = TypeHintFrequency.WhenNecessary, typeHint = "_t")
+  }
+
   private val collection = MongoConnection()("silhouette")("silhouette.user")
 
   private def col: Future[JSONCollection] =
-    reactiveMongoApi.database.map(_.collection("goal"))
+    reactiveMongoApi.database.map(_.collection("silhouette.user"))
 
   def find(id: String): Future[Option[User]] =
     col.flatMap(_.find(BSONDocument("userId" -> id)).one[User])
@@ -47,7 +43,27 @@ class UserRepository @Inject() (
       "avatarURL" -> user.avatarURL,
       "activated" -> user.activated,
       "goal" -> goal,
-      "wTime" -> user.wTime
+      "sTime" -> user.sTime,
+      "wTime" -> user.wTime,
+      "oTime" -> user.oTime
+    ))
+  }
+
+  def updateTime(id: String, user: User, goal: Goal, sTime: Int, wTime: Int, oTime: Int) = {
+    val query = MongoDBObject("userId" -> id)
+    collection.update(query, MongoDBObject(
+      "userId" -> user.userID,
+      "loginInfo" -> user.loginInfo,
+      "firstName" -> user.firstName,
+      "lastName" -> user.lastName,
+      "fullName" -> user.fullName,
+      "email" -> user.email,
+      "avatarURL" -> user.avatarURL,
+      "activated" -> user.activated,
+      "goal" -> goal,
+      "sTime" -> sTime,
+      "wTime" -> wTime,
+      "oTime" -> oTime
     ))
   }
 }
