7658ec344b35331d46870c86d6a52d986d8afb4c
tomoyaueno29 <u.t012807@gmail.com>
2020-09-22 10:42:41 +0900

    Update UsersGoalForm.scala

--- app/forms/UsersGoalForm.scala
+++ app/forms/UsersGoalForm.scala
@@ -5,15 +5,18 @@ import play.api.data.Forms.{ mapping, of }
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.data.format.Formats.doubleFormat
+import play.data.format.Formats.DateTime
 
 object UsersGoalForm {
 
-  var form = Form(
+  var usersGoalForm = Form(
     mapping(
       "usersGoalID" -> text,
       "user_id" -> text,
       "goal_id" -> text,
-      "learning_time" -> of(doubleFormat)
+      "stack_time" -> of(doubleFormat),
+      "learning_time" -> of(doubleFormat),
+      "created_at" -> jodaDate
     )(UsersGoal.apply)(UsersGoal.unapply)
   )
 
