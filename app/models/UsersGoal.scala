93e0dfec6206d53933c0e804aa4508c82e28d54c
tomoyaueno29 <u.t012807@gmail.com>
2020-09-22 10:42:43 +0900

    Update UsersGoal.scala

--- app/models/UsersGoal.scala
+++ app/models/UsersGoal.scala
@@ -2,6 +2,8 @@ package models
 
 import java.util.UUID
 
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 import reactivemongo.bson.BSONObjectID
 import play.api.libs.functional.syntax.unlift
 import play.api.libs.functional.syntax._
@@ -12,7 +14,9 @@ case class UsersGoal(
   usersGoalID: String = UUID.randomUUID.toString,
   user_id: String,
   goal_id: String,
-  learning_time: Double
+  stack_time: Double,
+  learning_time: Double,
+  created_at: DateTime
 )
 
 object UsersGoal {
@@ -30,12 +34,42 @@ object UsersGoal {
     }
   }
 
-  implicit val goalFormat: OFormat[UsersGoal] = (
-    (JsPath \ "usersGoalID").format[String] and
-    (JsPath \ "user_id").format[String] and
-    (JsPath \ "goal_id").format[String] and
-    (JsPath \ "learning_time").format[Double]
-  )(UsersGoal.apply, unlift(UsersGoal.unapply))
+  val dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
 
-  //  implicit val goalFormat: OFormat[User_goals] = Json.format[User_goals]
+  val jodaDateReads = Reads[DateTime](js =>
+    js.validate[String].map[DateTime](dtString =>
+      DateTime.parse(dtString, DateTimeFormat.forPattern(dateFormat))))
+
+  val jodaDateWrites: Writes[DateTime] = new Writes[DateTime] {
+    def writes(d: DateTime): JsValue = JsString(d.toString())
+  }
+
+  val usersGoalReads: Reads[UsersGoal] = (
+    (JsPath \ "usersGoalID").read[String] and
+    (JsPath \ "user_id").read[String] and
+    (JsPath \ "goal_id").read[String] and
+    (JsPath \ "stack_time").read[Double] and
+    (JsPath \ "learning_time").read[Double] and
+    (JsPath \ "created_at").read[DateTime](jodaDateReads)
+  )(UsersGoal.apply _)
+
+  val usersGoalWrites: Writes[UsersGoal] = (
+    (JsPath \ "usersGoalID").write[String] and
+    (JsPath \ "user_id").write[String] and
+    (JsPath \ "goal_id").write[String] and
+    (JsPath \ "stack_time").write[Double] and
+    (JsPath \ "learning_time").write[Double] and
+    (JsPath \ "created_at").write[DateTime](jodaDateWrites)
+  )(unlift(UsersGoal.unapply))
+
+  //  implicit val goalFormat: OFormat[UsersGoal] = (
+  //    (JsPath \ "usersGoalID").format[String] and
+  //    (JsPath \ "user_id").format[String] and
+  //    (JsPath \ "goal_id").format[String] and
+  //    (JsPath \ "stack_time").format[Double] and
+  //    (JsPath \ "learning_time").format[Double] and
+  //    (JsPath \ "created_at").format[DateTime](jodaDateWrites)(jodaDateReads)
+  //  )(UsersGoal.apply, unlift(UsersGoal.unapply))
+
+  implicit val goalFormat: OFormat[UsersGoal] = Json.format[UsersGoal]
 }
